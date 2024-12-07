a83b8a79118b9a0a1fd376ab4d1ba5a6f28fe558
Tong Yang <tongyang80@yahoo.com>
2024-12-07 22:16:21 +0000

    update todocreate component to handle submits.

--- src/components/TodoCreate.jsx
+++ src/components/TodoCreate.jsx
@@ -1,7 +1,30 @@
-const TodoCreate = () => {
+import { useState } from 'react';
+
+const TodoCreate = (createToDo) => {
+  const [title, setTitle] = useState('');
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createToDo(title);
+    setTitle('');
+  };
+
   return (
     <>
-      <div>Todo Create</div>
+      <form onSubmit={handleSubmit} className="todo-create">
+        <input
+          type="text"
+          name="title"
+          id="title"
+          placeholder="Add a Task"
+          value={title}
+          onChange={handleChange}
+        />
+      </form>
     </>
   );
 };
