751f20a69b6807b30bf5bc2cc083a2d6d67621cd
Tong Yang <tongyang80@yahoo.com>
2024-12-07 22:44:04 +0000

    update todo show component.

--- src/components/TodoShow.jsx
+++ src/components/TodoShow.jsx
@@ -1,7 +1,52 @@
-const TodoShow = () => {
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
+import TodoEdit from './TodoEdit.jsx';
+import DeleteIcon from '../../src/delete.png'
+import EditIcon from '../../src/edit.png'
+
+const TodoShow = ({ todo, removeTodo, changeToDo }) => {
+  const [showEdit, setShowEdit] = useState(false);
+
+  const handleDelete = () => {
+    removeTodo(todo.id)
+  }
+
+  const handleEdit = () => {
+    setShowEdit(true);
+  };
+
+  const handleDoubleClick = () => {
+    changeToDo(todo.id, todo.title, !todo.completed);
+  };
+
+  const handleSubmit = (id, title) => {
+    changeToDo(id, title);
+    setShowEdit(false);
+  };
+
+  if (showEdit) {
+    return (
+      <>
+        <li className="todo">
+          <TodoEdit todo={todo} onSubmit={handleSubmit} />
+        </li>
+      </>
+    )
+  }
+
   return (
     <>
-      <div>Todo show</div>
+      <li className="todo" onDoubleClick={handleDoubleClick}>
+        <p className={todo.completed && 'completed'}>{todo.title}</p>
+        <div className="actions">
+          <button onClick={handleDelete}>
+            <img src={DeleteIcon} title="Delete" />
+          </button>
+          <button onClick={handleEdit}>
+            <img src={EditIcon} title="Edit" />
+          </button>
+        </div>
+      </li>
     </>
   );
 };
