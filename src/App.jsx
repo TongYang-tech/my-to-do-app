351611f4e964602831d3e96fe5713ca1793fd087
Tong Yang <tongyang80@yahoo.com>
2025-02-18 21:23:05 -0600

    update readme file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,7 @@
 import TodoList from './components/TodoList';
 import TodoCreate from './components/TodoCreate';
 import './App.css';
-import { useState } from 'react';
-
 const App = () => {
-  const [todos, setTodos] = useState([]);
-
-  const createTodo = (title) => {
-    const newTodo = { id: crypto.randomUUID(), title, completed: false };
-    const updatedTodos = [...todos, newTodo];
-    setTodos(updatedTodos);
-  };
-
-  const removeTodo = (id) => {
-    const updatedTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(updatedTodos);
-  };
-
-  const changeTodo = (id, title, completed = false) => {
-    const updatedTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, title, completed };
-      }
-      return todo;
-    });
-
-    setTodos(updatedTodos);
-  };
-
   return (
     <main className="main">
       <h1>
