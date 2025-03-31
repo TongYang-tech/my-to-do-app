a31c68d773164fe70b6e26aae0a031fb541afb1b
Tong Yang <tongyang80@yahoo.com>
2025-03-31 16:19:11 -0500

    add basic SSG.

--- server/src/app.js
+++ server/src/app.js
@@ -3,9 +3,16 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import config from 'config'
 import dotenv from 'dotenv'
-
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { initDb } from './db/database.js'
 import router from './routes/index.js'
+
+const dist = '/client/dist'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const buildPath = path.join(__dirname, `../../${dist}`)
+const assetsPath = path.join(__dirname, `../../${dist}/assets`)
 
 const basePath = config.get('basePath')
 const app = express()
@@ -18,5 +25,12 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(basePath, router)
+app.use(`${basePath}/assets`, express.static(assetsPath, { index: false }))
+
+app.get('*', (req, res) => {
+  res.sendFile('index.html', {
+    root: buildPath
+  })
+})
 
 export default app
