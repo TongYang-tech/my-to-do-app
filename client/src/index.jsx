63c96e2f95440c194cb4ca8205bed98d168a8cb5
Tong Yang <tongyang80@yahoo.com>
2025-03-31 20:34:13 -0500

    init ssr.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot, hydrateRoot } from 'react-dom/client'
 import App from './components/Application'
 import './styles/index.scss'
 
-createRoot(document.getElementById('root')).render(
+hydrateRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
